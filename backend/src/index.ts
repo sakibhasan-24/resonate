0edc1c707de6d4b3db6c3ac2a35054e014197d30
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-01-17 01:02:50 +0600

    backend setup

---
+++ backend/src/index.ts
@@ -0,0 +1,2 @@
+console.log("Hello(Hey Wait stil more work to go) world!");
+// // "dev": "ts-node src/index.ts",
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
 function App() {
   return (
     <>
       <h1 className="text-red-200">Hello melody</h1>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </>
   );
 }
